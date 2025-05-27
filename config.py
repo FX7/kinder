6c5021679fc764e56c5bba1d77ccf70eed225f67
effex7 <effex7@posteo.de>
2025-05-27 19:14:45 +0000

    Logging

--- config.py
+++ config.py
@@ -1,9 +1,25 @@
 import os
+import logging
 
 from dotenv import load_dotenv
 
 class Config:
     load_dotenv()
+
+    level = logging.getLevelName(os.environ.get('KT_LOG_LEVEL', 'INFO'))
+    logging.basicConfig(level=level,  # Setze das Logging-Level
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  # Format der Log-Nachricht
+        datefmt='%Y-%m-%d %H:%M:%S')  # Datumsformat
+
+    external_logger = logging.getLogger('smbprotocol')
+    external_logger.setLevel(logging.WARNING)
+    external_logger = logging.getLogger('urllib3')
+    external_logger.setLevel(logging.WARNING)
+    external_logger = logging.getLogger('spnego')
+    external_logger.setLevel(logging.WARNING)
+    external_logger = logging.getLogger('smbclient')
+    external_logger.setLevel(logging.WARNING)
+
     SECRET_KEY = os.environ.get('KT_SERVER_SECRET_KEY', 'secret_key')
     SQLALCHEMY_DATABASE_URI = os.environ.get('KT_DATABASE_URI', 'sqlite:////data/database.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
