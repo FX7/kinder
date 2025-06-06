914a5d9b34b61e94270e7ea5c63783dd2743648f
effex7 <effex7@posteo.de>
2025-06-06 20:38:03 +0000

    unused

--- mail.py
+++
@@ -1,8 +0,0 @@
-from flask_mail import Mail, Message
-
-mail = Mail()
-
-def send_email(subject, recipient, body):
-    msg = Message(subject, recipients=[recipient])
-    msg.body = body
-    mail.send(msg)
