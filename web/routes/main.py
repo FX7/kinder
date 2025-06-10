4e344fe926e15c5c4b97d70c1969579b73158842
effex7 <effex7@posteo.de>
2025-06-10 17:02:25 +0000

    prepare license route

--- web/routes/main.py
+++ web/routes/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import render_template, Blueprint
 
 bp = Blueprint('main', __name__)
@@ -5,3 +6,10 @@ bp = Blueprint('main', __name__)
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+@bp.route('/LICENSE')
+def license():
+    with open('LICENSE', 'r') as file:
+        content = file.read()
+    
+    return content, 200
