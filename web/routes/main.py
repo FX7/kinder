91036a85abe5a75b1ceedc11c66f8b738427aef5
effex7 <effex7@posteo.de>
2025-06-12 16:41:57 +0000

    Now multiple path and multiple sites

--- web/routes/main.py
+++ web/routes/main.py
@@ -7,6 +7,20 @@ bp = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
+@bp.route('/vote')
+def vote():
+    return render_template('vote.html')
+
+# @bp.route('/status')
+# def status():
+#     with open('LICENSE', 'r', encoding='utf-8') as licenseFile:
+#         license = _licenseFormat(licenseFile.read())
+
+#     with open('web/static/bootstrap/LICENSE', 'r', encoding='utf-8') as bootstrapFile:
+#         bootstrap = _licenseFormat(bootstrapFile .read())
+
+#     return render_template('about.html', license=license, bootstrap=bootstrap)
+
 @bp.route('/about')
 def about():
     with open('LICENSE', 'r', encoding='utf-8') as licenseFile:
