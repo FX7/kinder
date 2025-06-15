3f2e5a7b14a65427d0745fdc02612b4ff051b50f
effex7 <effex7@posteo.de>
2025-06-15 10:01:29 +0000

    unused

--- web/routes/main.py
+++ web/routes/main.py
@@ -11,16 +11,6 @@ def index():
 def vote():
     return render_template('vote.html')
 
-# @bp.route('/status')
-# def status():
-#     with open('LICENSE', 'r', encoding='utf-8') as licenseFile:
-#         license = _licenseFormat(licenseFile.read())
-
-#     with open('web/static/bootstrap/LICENSE', 'r', encoding='utf-8') as bootstrapFile:
-#         bootstrap = _licenseFormat(bootstrapFile .read())
-
-#     return render_template('about.html', license=license, bootstrap=bootstrap)
-
 @bp.route('/about')
 def about():
     with open('LICENSE', 'r', encoding='utf-8') as licenseFile:
