7a4c2e7c04ab4bf5aebaf037b8809033bec76572
effex7 <effex7@posteo.de>
2025-07-29 23:25:29 +0000

    https://github.com/FX7/kinder/issues/89

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -21,11 +21,19 @@ if [ "$?" != "0" ]; then
   exit 1
 fi
 
-# Link the /cache dir to the browser accessable cache dir
+# link the /cache dir to the browser accessable cache dir
 if [ -d /app/web/static/images/cache ]; then
   rm -R /app/web/static/images/cache
 fi
-
 ln -s $KT_CACHE_FOLDER /app/web/static/images/cache
+
+# provide or link the username suggestions to the browser accessable file
+if [ -f $KT_DATA_FOLDER/usernames.json ]; then
+  rm /app/web/static/data/usernames.json
+else
+  mv /app/web/static/data/usernames.json $KT_DATA_FOLDER/usernames.json
+fi
+ln -s $KT_DATA_FOLDER/usernames.json /app/web/static/data/usernames.json
+
 
 exec "$@"
